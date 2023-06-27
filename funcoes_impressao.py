923e0f5e6cfbf4ea61a349cdf3ba09a5bfc3162b
Renata L. R. Sena <renatalrs@ufba.br>
2023-06-27 10:56:08 -0300

    Update funcoes_impressao.py

--- funcoes_impressao.py
+++ funcoes_impressao.py
@@ -10,23 +10,44 @@ def imprimir_menu():
   print("3. Sair\n")
 
 def imprimir_estatisticas(time,ano='todos'):
-  time,vitorias, derrotas, empates, gols_mandante, gols_visitante, gols_contra_mandante, gols_contra_visitante = obter_estatisticas(time,ano)
-  print('\n--------------------------------------------------')
-  print('\nEstatísticas do Brasileirão Série A de 2003 a 2019')
-  print('\n--------------------------------------------------')
+  try:
+    time,vitorias, derrotas, empates, *gols = obter_estatisticas(time,ano)
+    partidas = vitorias+empates+derrotas
+    gols_mandante = gols[0]
+    gols_visitante = gols[1]
+    gols_contra_mandante = gols[2]
+    gols_contra_visitante = gols[3]
+    gols_pro = gols_mandante+gols_visitante
+    gols_contra = gols_contra_mandante + gols_contra_visitante
+    saldo_gols = gols_pro - gols_contra
+    
+    print('\n--------------------------------------------------')
+    print('\nEstatísticas do Brasileirão Série A de 2003 a 2019')
+    print('\n--------------------------------------------------')
 
-  if ano == 'todos':
-    print(f'\nAs estatísticas do time {time.title()} de 2003 a 2019 são:\n')
-  else:
-    print(f'\nAs estatísticas do time {time.title()} em {ano} são:\n')
+    if ano == 'todos':
+      print(f'\nAs estatísticas do time {time.title()} de 2003 a 2019 são:\n')
+    else:
+      print(f'\nAs estatísticas do time {time.title()} em {ano} são:\n')
+      print(f'Partidas: {partidas}')
+      print(f'Vitórias: {vitorias}')
+      print(f'Derrotas: {derrotas}')
+      print(f'Empates: {empates}')
+      print('--------------------------------------------------')
+      print(f'Gols Pro: {gols_pro}')
+      print(f'Gols Contra: {gols_contra}')
+      print(f'Gols como Mandante: {gols_mandante}')
+      print(f'Gols como Visitante: {gols_visitante}')
+      print(f'Saldo de Gols: {saldo_gols}')
+
+      if partidas == 0:
+        print('Não foram disputadas partidas.')
+
+      media_gols_pro = gols_pro / partidas
+      media_gols_contra = gols_contra / partidas
+      print(f'Média de Gols Pro por partida: {round(media_gols_pro, 2)}')
+      print(f'Média de Gols Contra por partida: {round(media_gols_contra, 2)}')
+  except ZeroDivisionError:
+    print('Erro: Não é possível calcular as médias sem partidas disputadas.')
+
   print('--------------------------------------------------')
-  print(f'Partidas: {vitorias+derrotas+empates}')
-  print(f'Vitórias: {vitorias}')
-  print(f'Derrotas: {derrotas}')
-  print(f'Empates: {empates}')
-  print('--------------------------------------------------')
-  print(f'Gols Pro: {gols_mandante+gols_visitante}')
-  print(f'Gols Contra: {gols_contra_mandante+gols_contra_visitante}')
-  print(f'Gols como Mandante: {gols_mandante}')
-  print(f'Gols como Visitante: {gols_visitante}')
-  print(f'Saldo de Gols: {(gols_mandante+gols_visitante)-(gols_contra_mandante+gols_contra_visitante)}')
